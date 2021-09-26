4f05bf1e7db1cff419146fbbb7504a4e28468c08
Aaron McUmber, Ph.D <aaron.mcumber@gmail.com>
2021-09-26 13:42:53 -0700

    LR seems promising

--- tennis_agents/plotting.py
+++ tennis_agents/plotting.py
@@ -15,10 +15,11 @@ def plot_scores(scores, i_map=0):
     score_df = pd.DataFrame({'scores': scores})
     score_df = score_df.assign(mean=lambda df: df.rolling(10).mean()['scores'])
 
-    _ ,ax = plt.subplots(1,1, figsize=(10,8))
+    fig ,ax = plt.subplots(1,1, figsize=(10,8))
 
     ax = score_df.plot(ax=ax, color=cmap[2*(i_map%4):])
     ax.set_title('DDPG Scores vs Time for Tennis')
     ax.set_xlabel('Episode #')
     ax.set_ylabel('Score')
     plt.show()
+    return fig
